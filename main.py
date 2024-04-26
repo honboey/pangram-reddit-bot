236f3bda044891a02adec22bf4f2bf97c29a7c4f
Hon Boey <hon@dialogicstudios.com.au>
2024-04-26 19:39:05 +1000

    Create instance of praw using proper credentials

--- main.py
+++ main.py
@@ -5,10 +5,14 @@ from dotenv import load_dotenv
 # Load .env variables for authentication
 load_dotenv()
 reddit_client_id = os.getenv("REDDIT_CLIENT_ID")
-reddit_client_secret = os.getenv("REDDIT_CLIENT_ID")
-reddit_client_id = os.getenv("REDDIT_CLIENT_ID")
-reddit_client_id = os.getenv("REDDIT_CLIENT_ID")
+reddit_client_secret = os.getenv("REDDIT_CLIENT_SECRET")
+reddit_username = os.getenv("REDDIT_USERNAME")
+reddit_password = os.getenv("REDDIT_PASSWORD")
 
 reddit = praw.Reddit(
-  client_id=REDDIT
+  client_id=reddit_client_id,
+  client_secret=reddit_client_secret,
+  password=reddit_password,
+  username=reddit_username,
+  user_agent="pangram_detector_bot:v1"
 )
